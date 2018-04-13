8856d53f9fecce255c2dcd1a3342b4a962407867
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-12 21:45:45 -0300

    Adding sound to the rocket, delimitating the model movement to 2d

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,32 +6,55 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 	
 	Rigidbody rigidbody; //Cria uma variável global do tipo Rigidbody
-
-	// Use this for initialization
+	
+	AudioSource audiosource; // Cria uma variável global do tipo AudioSource
+	
 	void Start () {
 		rigidbody = GetComponent<Rigidbody>(); //Age apenas em componentes do tipo Rigidbody
+		audiosource = GetComponent<AudioSource>(); //Age apenas em compentes do tipo AudioSource
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ProcessInput(); //Invoca a função que processa entradas a cada update
+		Thrust(); //Invoca a função que processa a entrada Espaço
+		Rotate(); //Invoca a função que processa entradas Esq e Dir
 	}
 
-    private void ProcessInput()
+	private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        { //se a tecla está pressionada
+            rigidbody.AddRelativeForce(Vector3.up); //Gera uma força relativa ao objeto
+            print("Propulsion");
+            if (!audiosource.isPlaying) //Se audio não estiver tocando
+            {
+                audiosource.Play(); //Tocar audio
+            }
+        }
+        else // Se espaço não estiver pressionado
+        {
+            audiosource.Stop(); //Parar audio
+        }
+    }
+
+    private void Rotate()
     {
         
-		if(Input.GetKey(KeyCode.Space)) { //se a tecla está pressionada
-			rigidbody.AddRelativeForce(Vector3.up); //Gera uma força relativa ao objeto
-			print("Propulsão");
-		}
-		 if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.Rotate(Vector3.forward);
-			print("Esquerda");
-		}
-		else if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.Rotate(Vector3.back);
-			print("Direita");
-		}
-		
+		rigidbody.freezeRotation = true; //Controle manual de rotação 
+        
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward); //Força na direção positiva de Z (Regra de fluxo da mão esquerda)
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back);
+            print("Right");
+        }
+
+		rigidbody.freezeRotation = false; //Controle físico da rotação
+
     }
+
 }

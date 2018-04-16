3d14033838e10c5210b451a960694fe0bc153851
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-15 21:44:04 -0300

    Stylizing levels, creating states for the ship and linking levels

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
-	
 	Rigidbody rigidbody; //Cria uma variável global do tipo Rigidbody
-	
 	AudioSource audiosource; // Cria uma variável global do tipo AudioSource
-	
     [SerializeField]float rThrust; //Cria um campo float editavel dentro do unity (Giro)
-    
     [SerializeField]float mThrust; //Cria um campo float editavel dentro do unity (Aceleração)
+
+    enum State {Alive, Dying, Transcending};
+    State state = State.Alive;
+
 	void Start () {
 		rigidbody = GetComponent<Rigidbody>(); //Age apenas em componentes do tipo Rigidbody
 		audiosource = GetComponent<AudioSource>(); //Age apenas em compentes do tipo AudioSource
@@ -24,26 +22,36 @@ public class Rocket : MonoBehaviour {
 	}
 
     void OnCollisionEnter(Collision collision) {
+        
+        if(state != State.Alive)
+            return; //Só computa colisões se estiver vivo
+
         switch (collision.gameObject.tag){
             case "Friendly":
                 print("Okay.");
                 break;
-            
-            case "Unfriendly":
-                print("Dead!");
+
+            case "Finish":
+                print("Level Finished!");
+                state = State.Transcending;
+                Invoke("LoadNextScene",1f); //Carrega a proxima fase após 1 segundo
                 break;
 
-            case "Fuel":
-                print("Fuelled Up!");
+            default:
+                print("Dead!");
+                state = State.Dying;
+                Invoke("LoadFirstLevel",1f);
                 break;
         }
     }
 
-	private void Thrust()
+    
+
+    private void Thrust()
     {
         float mainThisFrame = mThrust * Time.deltaTime; //normaliza a aceleração com base nos frames
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && state == State.Alive)
         { //se a tecla está pressionada
             rigidbody.AddRelativeForce(Vector3.up * mainThisFrame); //Gera uma força relativa ao objeto
             print("Propulsion");
@@ -56,28 +64,39 @@ public class Rocket : MonoBehaviour {
         {
             audiosource.Stop(); //Parar audio
         }
+
     }
 
     private void Rotate()
     {
-        
-		rigidbody.freezeRotation = true; //Controle manual de rotação 
-        
-        float rotationThisFrame = rThrust * Time.deltaTime; //normaliza a aceleração com base nos frames
+        if(state == State.Alive) {
+            rigidbody.freezeRotation = true; //Controle manual de rotação 
+            
+            float rotationThisFrame = rThrust * Time.deltaTime; //normaliza a aceleração com base nos frames
 
-		if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(Vector3.forward * rotationThisFrame); //Força na direção positiva de Z (Regra de fluxo da mão esquerda)
-            print("Left");
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(Vector3.forward * rotationThisFrame); //Força na direção positiva de Z (Regra de fluxo da mão esquerda)
+                print("Left");
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(Vector3.back * rotationThisFrame);
+                print("Right");
+            }
+
+            rigidbody.freezeRotation = false; //Controle físico da rotação
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(Vector3.back * rotationThisFrame);
-            print("Right");
-        }
+    }
 
-		rigidbody.freezeRotation = false; //Controle físico da rotação
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
 
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }

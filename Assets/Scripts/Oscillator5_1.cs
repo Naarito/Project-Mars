98a71dbb6c4fe76ae735886e622ba661663b015f
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-05-03 00:06:59 -0300

    Created new levels, implemented lightining difference across levels

---
+++ Assets/Scripts/Oscillator5_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator5_1 : MonoBehaviour {
+
+	Vector3 movementVector = new Vector3 (0f,-10f,0f); //Vetor de deslocamento, usar [SerializeField] Vector3 movementVector; para experimentar outros vetores
+	float period = 8f; //Periodo de 6 segundos
+	Vector3 startingPos;
+
+	void Start () {
+		startingPos = transform.position;
+	}
+	
+	void Update () {
+		if (period <= Mathf.Epsilon) return; //Cria uma condição onde o periodo não pode ser inferior a zero, epsilon representa do menor float possível
+
+		float cycles = Time.time / period; //Cresce continuamente, a partir de zero.
+
+		const float tau = Mathf.PI * 2; //6.28 ou 2Pi, circulo completo
+		float rawSinWave = Mathf.Sin(cycles * tau); // multiplica o numero do ciclo por uma volta completa senoidal
+		//float movementFactor = (rawSinWave + 1)/2; //pega o valor de -1 a 1, transforma em 0 a 2 e divide por 0.5, resultando em 0 a  1
+
+		Vector3 offset = movementVector * rawSinWave; //movementFactor; //A cada update armazena o valor do fator e multiplica pelo vetor de movimento
+		transform.position = startingPos + offset; //soma a posição inicial o valor do offset gerado na linha acima
+	}
+}
